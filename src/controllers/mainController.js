c9b9339bbfd3b98985166f6f4f708ac75070589d
joeecasa <joeecasa@gmail.com>
2022-05-10 10:16:07 -0300

    primer commit falta terminar

---
+++ src/controllers/mainController.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const visited = products.filter(function (product) {
+	return product.category == "visited";
+})
+const inSale = products.filter(function (product) {
+	return product.category == "in-sale";
+})
+
+
+
+const controller = {
+	index: (req, res) => {
+		const product = products.filter(item => item.id == req.param.id)
+
+		res.render("index.ejs", { 
+			visited: visited, 
+			inSale: inSale })
+	},
+	search: (req, res) => {
+		let busqueda = []
+		let formu = req.query.keywords
+		products.forEach(product => {
+			if (product.name.includes(formu) ) {
+				 busqueda.push(product)
+			}
+			
+		});
+		res.render("results.ejs", {busqueda:busqueda})
+	},
+};
+
+module.exports = controller;
---
+++ src/controllers/productsController.js
@@ -0,0 +1,51 @@
+const { privateDecrypt } = require('crypto');
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	// Root - Show all products
+	index: (req, res) => {
+
+		res.render("products.ejs", { products:products })
+	},
+
+	// Detail - Detail from one product
+	detail: (req, res) => {
+		let product = products.find(product => product.id == req.params.id)
+		res.render("detail.ejs",{product:product})
+	},
+
+	// Create - Form to create
+	create: (req, res) => {
+
+		res.render("product-create-form.ejs")
+	},
+
+	// Create -  Method to store
+	store: (req, res) => {
+		res.render("")
+	},
+
+	// Update - Form to edit
+	edit: (req, res) => {
+		let product = products.find(item => item.id == req.params.id)
+
+		res.render("product-edit-form", { product: product })
+	},
+	// Update - Method to update
+	update: (req, res) => {
+		res.send("producto updated")
+	},
+
+	// Delete - Delete one product from DB
+	destroy: (req, res) => {
+		res.send("producto eliminado")
+	}
+};
+
+module.exports = controller;
