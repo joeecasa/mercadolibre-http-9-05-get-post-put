ff0ee66ad70f51d2cf8a3671d3ee2dbebe16b994
joeecasa <joeecasa@gmail.com>
2022-05-11 19:02:53 -0300

    mejorando el sitio

---
+++ a.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+
+
+const productsFilePath = path.join(__dirname, 'src/data/productsDataBase.json');
+const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+console.log(productsFilePath)
+
+
+let array=[0, 2, 1, 5, 
+    {id:55 }];  
+let ultimo = array.length -1 
+console.log(array[ultimo].id)
+
+
+
+
+let ultimoi = products.length -1  
+
+console.log("ultomo id",products[ultimoi].id) 
+
+console.log(ultimoi)
+
+let idnuevo = products[ultimoi].id + 1
+
+
+console.log(idnuevo);
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,9 +1,11 @@
 const { privateDecrypt } = require('crypto');
 const fs = require('fs');
 const path = require('path');
-
-const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+//para estos 3 no se puede usar const ya que mas abajo cuando reescribamos los json hay q volver a declarar las variables
+// y las variables declaradas con const no se pueden modificar
+let productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+let productJSON = fs.readFileSync(productsFilePath, 'utf-8')
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
 
 const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 
@@ -11,25 +13,55 @@ const controller = {
 	// Root - Show all products
 	index: (req, res) => {
 
-		res.render("products.ejs", { products:products })
+		res.render("products.ejs", { products: products })
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
 		let product = products.find(product => product.id == req.params.id)
-		res.render("detail.ejs",{product:product})
+		res.render("detail.ejs", { product: product })
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-
 		res.render("product-create-form.ejs")
 	},
 
 	// Create -  Method to store
 	store: (req, res) => {
+		let ultimo = products.length -1  
+		let idnuevo = products[ultimo].id + 1
 
-		res.send("producto creado")
+		let productoForm = {
+			id: idnuevo,
+            name: req.body.name,
+            price: req.body.price,
+            discount: req.body.discount,
+			category : req.body.category,
+			description : req.body.description
+
+        }
+		    // primero : leer que cosas ya habia por que si yo ya tenia usuarios registrados no quiero pisarlos
+        //let archivoUsuario = fs.readFileSync("usuarios.json", { encoding: "utf-8" })
+		let productosNuevo
+		// el archivo usuarios.json puede estar vacio( este es el caso ) por eso hacemos un if para q arranque vacio el array
+		if (productJSON == "") {
+			productosNuevo = []
+		} else {
+			// por el contrario, si el archivo ya tenia contenido lo descomprimimos pasandolo con json.parse a objeto literal para poder
+			//manipularlo con JS
+			productosNuevo = JSON.parse(productJSON)
+		}
+
+		productosNuevo.push(productoForm)
+
+		// ahora ya tenemos el array con los usuarios pero es justamente un array entonces lo pasamos a json de nuevo
+		products = JSON.stringify(productosNuevo)
+		// una vez terminado este proceso si lo podemos escribir y siempre reemplaza pero esta creando todo el tiempo
+		// un array nuevo con lo que hicimos arriba entonces no hay problema con usar la funcion write
+		fs.writeFileSync(productsFilePath, products)
+
+		res.redirect("/")
 	},
 
 	// Update - Form to edit
